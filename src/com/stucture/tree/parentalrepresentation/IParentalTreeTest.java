dd71236c6f7960c6989bf2c7c1ae549c5fd54561
liuenci <154910381@qq.com>
2018-03-04 18:48:40 +0800

    完成双亲表示法的学习

--- src/com/stucture/tree/parentalrepresentation/IParentalTreeTest.java
+++ src/com/stucture/tree/parentalrepresentation/IParentalTreeTest.java
@@ -1,5 +1,8 @@
 package com.stucture.tree.parentalrepresentation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * created by cier on 2018/3/4 15:05
  */
@@ -30,20 +33,44 @@ public class IParentalTreeTest {
 
         // 输出根节点
         System.out.println("根节点为:" + tree.getRoot());
+        System.out.println("====================================");
 
         // 输出树的深度
-        System.out.println("树的深度为:"+tree.getDeepth());
+        System.out.println("树的深度为:" + tree.getDeepth());
+        System.out.println("====================================");
 
         // 获得某个节点的双亲节点
         // System.out.println("node5的双亲节点为:"+tree.getParent(root));
-        System.out.println("node1 的双亲节点为:"+tree.getParent(node1));
-        System.out.println("node2 的双亲节点为:"+tree.getParent(node2));
-        System.out.println("node3 的双亲节点为:"+tree.getParent(node3));
-        System.out.println("node4 的双亲节点为:"+tree.getParent(node4));
-        System.out.println("node5 的双亲节点为:"+tree.getParent(node5));
-        System.out.println("node6 的双亲节点为:"+tree.getParent(node6));
+        System.out.println("node1 的双亲节点为:" + tree.getParent(node1));
+        System.out.println("node2 的双亲节点为:" + tree.getParent(node2));
+        System.out.println("node3 的双亲节点为:" + tree.getParent(node3));
+        System.out.println("node4 的双亲节点为:" + tree.getParent(node4));
+        System.out.println("node5 的双亲节点为:" + tree.getParent(node5));
+        System.out.println("node6 的双亲节点为:" + tree.getParent(node6));
+        System.out.println("====================================");
 
+        // 获取树的全部节点
+        List<Node<String>> allNode = tree.getAllNode();
+        for (int i = 0; i < allNode.size(); i++) {
+            System.out.println(allNode.get(i));
+        }
+        System.out.println("====================================");
 
+        // 获取某个节点的所有子节点
+        List<Node<String>> childNode = tree.getChild(root);
+        for (int i = 0; i < childNode.size(); i++) {
+            System.out.println(childNode.get(i));
+        }
+        System.out.println("====================================");
+
+        // 获取指定节点的数组下标
+        System.out.println("root 的数组下标为："+tree.getPos(root));
+        System.out.println("root 的数组下标为："+tree.getPos(node1));
+        System.out.println("root 的数组下标为："+tree.getPos(node2));
+        System.out.println("root 的数组下标为："+tree.getPos(node3));
+        System.out.println("root 的数组下标为："+tree.getPos(node4));
+        System.out.println("root 的数组下标为："+tree.getPos(node5));
+        System.out.println("root 的数组下标为："+tree.getPos(node6));
 
     }
 }
