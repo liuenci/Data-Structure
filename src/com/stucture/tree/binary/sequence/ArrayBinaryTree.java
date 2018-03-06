d4ca0d3a1c01f12aac54ce8d94bb670b3eae341a
liuenci <154910381@qq.com>
2018-03-06 09:16:02 +0800

    完成二叉树的代码

--- src/com/stucture/tree/binary/sequence/ArrayBinaryTree.java
+++ src/com/stucture/tree/binary/sequence/ArrayBinaryTree.java
@@ -1,5 +1,7 @@
 package com.stucture.tree.binary.sequence;
 
+import java.util.Arrays;
+
 /**
  * created by cier on 2018/3/5 20:32
  */
@@ -57,6 +59,7 @@ public class ArrayBinaryTree<T> {
             System.out.println("树已满，无法添加结点");
         }
         if (left) {
+            // +1 是因为树的存储是从 0 开始的，所以少算了一位
             arr[2 * index + 1] = data; // 添加左子结点
         } else {
             arr[2 * index + 2] = data; // 添加右子节点
@@ -96,7 +99,63 @@ public class ArrayBinaryTree<T> {
     public T getLeft(int index) {
         if (2 * index + 1 >= size) {
             System.out.println("该结点为叶子结点，无子结点。");
+            return null;
         }
         return (T)arr[index * 2 + 1];
+    }
+
+    /**
+     * 返回指定节点(非叶子)的右子节点，当右子结点不存在时返回 null
+     * @param index
+     * @return
+     */
+    public T getRight(int index){
+        if (2 * index +1 >=size) {
+            System.out.println("该结点为叶子节点，无子结点");
+            return null;
+        }
+        return (T)arr[index * 2 + 2];
+    }
+
+    /**
+     * 返回该二叉树的深度
+     * @return
+     */
+    public int getDeepth() {
+        return deep;
+    }
+
+    /**
+     * 返回指定位置在数组中的位置
+     * @param data
+     * @return
+     */
+    public int index(T data) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i].equals(data)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 打印数组
+     * @return
+     */
+    public String toString() {
+        return Arrays.toString(arr);
+    }
+
+    public static void main(String[] args) {
+        ArrayBinaryTree<String> binaryTree = new ArrayBinaryTree<>(4,"根节点");
+        binaryTree.add(0,"第二层右子结点",false);
+        binaryTree.add(2,"第三层右子结点",false);
+        binaryTree.add(6,"第四层右子结点",false);
+        System.out.println("树的深度为:"+binaryTree.getDeepth());
+        System.out.println(binaryTree.getLeft(0));
+        System.out.println(binaryTree);
+        System.out.println(binaryTree.getRight(0));
+        System.out.println(binaryTree.index("根节点"));
     }
 }
